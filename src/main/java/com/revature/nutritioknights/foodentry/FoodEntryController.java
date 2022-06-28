5987097b85f72f73b5eea623bca0495579bceb2d
William Marquardt <william940@revature.net>
2022-06-28 11:51:03 -0500

    crossorigin for all methods on foodentry controller

--- src/main/java/com/revature/nutritioknights/foodentry/FoodEntryController.java
+++ src/main/java/com/revature/nutritioknights/foodentry/FoodEntryController.java
@@ -26,19 +26,23 @@ public class FoodEntryController {
     @Autowired
     public FoodEntryController(FoodEntryService foodEntryService) {this.foodEntryService = foodEntryService;
     }
+    @CrossOrigin
     @GetMapping(value = "/id={id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody FoodEntry getEntry(@PathVariable String id) {
         return foodEntryService.getById(id);
     }
+    @CrossOrigin
     @GetMapping(value = "/u={username}", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<Date> getActivity(@PathVariable String username) {
         return foodEntryService.getActivity(username);
     }
+    @CrossOrigin
     @GetMapping(value = "/d={date}", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<FoodEntry> getUserEntriesByDate(@PathVariable Long date, @RequestBody GetByDateRequest request) {
         request.setDateInt(date);
         return foodEntryService.getUserEntriesByDate(request);
     }
+    @CrossOrigin
     @GetMapping(value = "/suggest/{mealname_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<FoodEntry> getUserEntriesByMealname(@PathVariable int mealname_id, @RequestBody GetByMealnameRequest request) {
         request.setMealnameId(mealname_id);
@@ -52,7 +56,7 @@ public class FoodEntryController {
     public @ResponseBody String newEntry(@RequestBody NewFoodEntryRequest request) {
         return foodEntryService.newEntry(request);
     }
-
+    @CrossOrigin
     @DeleteMapping(value = "/{target}", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody void deleteEntry(@PathVariable String target){
         foodEntryService.deleteEntry(target);
